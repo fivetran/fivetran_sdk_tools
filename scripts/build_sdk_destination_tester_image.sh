1416df1d0e9f34d7b6e20a5e578ba68854fc41e6
emrah.diril <emrah.diril@fivetran.com>
2024-03-05 00:02:47 -0500

    Add platform to docker pull

--- scripts/build_sdk_destination_tester_image.sh
+++ scripts/build_sdk_destination_tester_image.sh
@@ -22,7 +22,7 @@ cp -f "$(git rev-parse --show-toplevel)/bazel-bin/testers/run_sdk_destination_te
 
 docker buildx build --push -f Dockerfile.destination_tester --platform=linux/amd64,linux/arm64 --tag fivetrandocker/sdk-destination-tester:$VERSION  .
 
-docker pull fivetrandocker/sdk-destination-tester:$VERSION
+docker pull --platform=linux/amd64 fivetrandocker/sdk-destination-tester:$VERSION
 
 # clean up
 rm run_sdk_destination_tester_deploy.jar
