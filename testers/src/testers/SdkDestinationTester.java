57e6006c2e3d82ebd0217831fc4c72af65f14d96
emrah.diril <emrah.diril@fivetran.com>
2024-04-10 16:53:05 +0300

    Update version number

--- testers/src/testers/SdkDestinationTester.java
+++ testers/src/testers/SdkDestinationTester.java
@@ -45,7 +45,7 @@ import picocli.CommandLine;
 public final class SdkDestinationTester {
     private static final Logger LOG = Logger.getLogger(SdkDestinationTester.class.getName());
 
-    private static final String VERSION = "024.0314.001";
+    private static final String VERSION = "024.0410.001";
 
     private static final CsvMapper CSV = createCsvMapper();
     private static final String DEFAULT_SCHEMA = "tester";
